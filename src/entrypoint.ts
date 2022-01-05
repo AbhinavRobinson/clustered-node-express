ceca1d6800b8867b2f0167ed617cbdc5f9a6bea9
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-01-05 17:30:01 +0530

    feat(cluster): add custom argvs and logging

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -2,18 +2,39 @@ import cluster from 'cluster'
 import os from 'os'
 import { worker } from './server'
 
-const port = 3000
-const pid = process.pid
+let port = 3000
+let loggerLevel = false
+let cpus = os.cpus().length
 
-if (cluster.isPrimary) {
-  const cpus = os.cpus().length
+try {
+  // get port from argv port or default to 3000
+  if (process.argv[2] === '--port') {
+    port = parseInt(process.argv[3]) || 3000
+    process.argv.splice(2, 2)
+  }
+  // get debug from argv debug
+  if (process.argv[2] === '--debug') {
+    loggerLevel = true
+    console.log('[master]: Debug mode enabled')
+    process.argv.splice(2, 1)
+  }
+  // get number of workers from argv workers or default to all
+  if (process.argv[2] === '--workers') {
+    cpus = parseInt(process.argv[3]) || os.cpus().length
+  }
+} catch (err) {
+  console.log('[master]: Invalid params')
+  process.exit(1)
+}
+
+if (cluster.isMaster) {
   for (let i = 0; i < cpus; i++) {
-    cluster.fork()
+    cluster.fork({ loggerLevel, port })
   }
   Object.values(cluster.workers).forEach((worker) => {
     worker.send(`${worker.id}`)
   })
-  console.log(`[primary:${pid}]: ${cpus} listening to ${port}`)
+  console.log(`[master]: ${cpus} listening to ${port}`)
 } else {
-  worker(port)
+  worker(parseInt(process.env.port), process.env.loggerLevel === 'true')
 }
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,11 @@ import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
 
-export const worker = (port: number) => {
+export const worker = (port: number, debug: boolean) => {
   const app = express()
   const pid = process.pid
   let wid = -1
   let calls = 0
-  let locked = false
 
   app.use(cors())
   app.use(helmet())
@@ -15,27 +14,27 @@ export const worker = (port: number) => {
   app.use(express.urlencoded({ extended: true }))
 
   app.use('/', (req, res) => {
-    setTimeout(() => {
-      res.send('Hello World!')
-      calls++
-      if (!locked) {
-        locked = true
-        Logger(wid, pid, calls)
-        setTimeout(() => {
-          locked = false
-        }, 5000)
-      }
-    }, 200)
+    // simulate work with for
+    for (let i = 0; i < 10000000; i++);
+    res.send('Hello World!')
+    calls++
   })
 
   app.listen(port)
 
-  process.on('message', (msg) => {
-    wid = parseInt(msg as string)
-    console.log(`[worker:${wid}:${pid}]: ${wid} listening to ${port}`)
-  })
+  debug &&
+    process.on('message', (msg) => {
+      wid = parseInt(msg as string)
+      console.log(`[worker:${wid}]: listening to ${port}`)
+    })
+
+  debug &&
+    setInterval(() => {
+      Logger(wid, pid, calls)
+      calls = 0
+    }, 5000)
 }
 
 const Logger = (wid: number, pid: number, calls: number) => {
-  console.log(`[worker:${wid}:${pid}]: Responding to ${calls} calls`)
+  console.log(`[worker:${wid}]: Responding to ${calls} calls last 5 seconds`)
 }
