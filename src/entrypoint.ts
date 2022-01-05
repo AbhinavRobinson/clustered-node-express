367ab36e7fbe3c131292e1044e0d358430a99214
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-01-05 14:19:27 +0530

    feat(cluster): add cluster process

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,1 +1,12 @@
-console.log('Hello Clusters')
+import cluster from 'cluster'
+import os from 'os'
+import { worker } from './server'
+
+if (cluster.isPrimary) {
+  const cpus = os.cpus().length
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
+} else {
+  worker(3000)
+}
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,22 @@ import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
 
-export const server = (port: number) => {
+export const worker = (port: number) => {
   const app = express()
+  const pid = process.pid
 
   app.use(cors())
   app.use(helmet())
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
 
-  app.listen(port, () => console.log(`❄️ App listening on port ${port}!`))
+  app.use('/', (req, res) => {
+    setTimeout(() => {
+      res.send('Hello World!')
+    }, 200)
+  })
+
+  app.listen(port, () =>
+    console.log(`❄️ Process:${pid} listening on port ${port}!`)
+  )
 }
